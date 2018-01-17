99cb69babc6da207bb43acf2c62fd761de949870
Gvs Akhil <gvs.akhil1997@gmail.com>
2018-01-17 19:49:18 +0530

    [Update] System Status now displays the selected status

--- website/public/js/sendState_updateJumbo.js
+++ website/public/js/sendState_updateJumbo.js
@@ -4,7 +4,11 @@ $(document).ready(function () {
         data.machine = "Machine1";
         data.status = this.innerText.trim();
         data.parent_mode = $(this).parents()[2].firstChild.nextSibling.innerText.trim();
-
+        document.getElementById("jumbotron").innerHTML = data.status;
+        var col = "blue";
+        if (data.parent_mode == "ON") col = "green";
+        else if (data.parent_mode == "FAILED") col = "red";
+        document.getElementById("jumbotron").style.color = col;
         $.ajax({
             type: 'POST',
             data: JSON.stringify(data),
