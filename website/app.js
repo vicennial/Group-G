79947108ec1d041047b4df881fbaeade19b78548
Saptarshi Ghosh <sghoshjr@gmail.com>
2018-01-21 18:31:13 +0530

    [Backend] Basic Login Authentication
    Basic Login Page
    
     Need MongoDB DB "isw" with a test user { uid : "Machine1", pwd : "test123" }
    
    See Issues for more details

--- website/app.js
+++ website/app.js
@@ -2,8 +2,13 @@ var express = require("express");
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
- 
-
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+var router = express.Router();
+var Schema = mongoose.Schema;
 
 //creating server
 var server = require('http').createServer(app);
@@ -31,3 +36,51 @@ app.post("/SendState", function(req, res) {
   console.log(JSON.stringify(req.body));
   res.send(req.body);
 });
+
+
+//Query MongoDB for Login Request
+
+//mongoose.model('isw',User);
+//mongoose.connect('mongodb://localhost:27017/');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/',function(req,res)
+{
+    res.render('index');
+});
+
+
+app.get('/MainPage',function(req,res)
+{
+	res.redirect('index.html');
+});
+
+app.get('/machine.html',function(req,res)
+{
+	res.redirect('index.html');
+});
+
+app.post('/MainPage', function(req, res)
+{
+	MongoClient.connect(url, function(err, db) {
+		if (err) throw err;
+		var dbo = db.db("isw");
+		var query = { uid: req.body.uid, pwd: req.body.pwd };
+		dbo.collection("isw").find(query).toArray(function(err, result) {
+			if (err) { 
+				throw err;
+			} else {
+			//console.log(result[0].uid);
+			if (result[0].uid == req.body.uid) {
+				console.log("Login Successful!");
+				res.redirect('/machine.html');
+			}
+			}
+			db.close();
+		});
+	});
+
+});
+
+
